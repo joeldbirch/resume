0fd6be17fd12a6330319f38b4613ca285a023a3d
Joel Birch <joeldbirch@gmail.com>
2020-02-16 13:45:31 +1100

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Joel Birch | Web Developer`,
+    author: `Joel Birch`,
+    description: `With 20+ years of experience in design, advertising and web development industries, I help advertising agencies from idea phase to final product build. I implement performant and accessible solutions, with a designer's eye for detail.`,
+    siteUrl: `https://joelbirch.design/`,
+    social: {
+      twitter: `joel_birch`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Joel Birch | Web Developer`,
+        short_name: `CV`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#38B2AC`,
+        display: `standalone`,
+        icon: 'src/assets/site-icon.png'
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true, // Enable tailwindcss support
+      }
+    }
+  ],
+};
