f07ce2f140a005f1b14816b35fd291287bc54080
Joel Birch <joeldbirch@gmail.com>
2020-02-16 16:59:44 +1100

    Customise summary.

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
-const Summary = ({ data }) => (
-  <section className="py-5 border-b border-neutral-300 lg:flex items-center">
-    <div className="my-5">
-      <img
-        className="rounded-full mx-auto w-32 lg:w-full xl:w-4/5"
-        src="./profile.jpg"
-        alt="profile"
-      />
-    </div>
-    <p className="text-center tracking-wide leading-relaxed lg:text-left lg:mx-8 lg:text-lg">
-      {data}
-    </p>
-  </section>
-);
+const Summary = ({ data }) => {
+  const paras = data.split(`\n\n`)
+  return (
+    <section className="py-5 border-b border-neutral-300 lg:flex">
+      <div className="my-5">
+        <img
+          className="mx-auto w-32 lg:w-full xl:w-4/5"
+          src="./profile.jpg"
+          alt="profile"
+          style={{
+            transform: `scalex(-1)`,
+            borderRadius: `100%`,
+          }}
+        />
+      </div>
+      <p className="tracking-wide leading-relaxed lg:mx-8 lg:text-lg">
+        {paras?.map(para => <p className="my-4">{para}</p>)}
+      </p>
+    </section>
+  )
+}
+
 
 export default Summary;
